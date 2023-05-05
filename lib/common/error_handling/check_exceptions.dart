c6830171edc1bf5345e181a7a1afd1e6aac2d76f
abolfazl <abolfazlmashhadi93@gmail.com>
2023-05-05 21:30:23 +0330

    added error handling

---
+++ lib/common/error_handling/check_exceptions.dart
@@ -0,0 +1,47 @@
+
+import 'package:dio/dio.dart';
+import 'package:news_reader/common/error_handling/app_exceptions.dart';
+import 'package:news_reader/common/resources/data_state.dart';
+
+
+class CheckExceptions {
+
+  static dynamic response(Response response) {
+    switch (response.statusCode) {
+      case 200:
+        return response;
+      case 400:
+        throw BadRequestException(response: response);
+      case 401:
+        throw UnauthorizedException();
+      case 404:
+        throw NotFoundException();
+      case 500:
+        throw ServerException();
+      default:
+        throw FetchDataException(message: "${response.statusCode}fetch exception");
+    }
+  }
+
+  static dynamic getError(AppException appException) async {
+    switch (appException.runtimeType) {
+      /// return error came from server
+      case BadRequestException:
+        return DataFailed(appException.message);
+
+      case NotFoundException:
+        return DataFailed(appException.message);
+      /// get refresh token and call api again
+      case UnauthorizedException:
+        return DataFailed(appException.message);
+
+      /// server error
+      case ServerException:
+        return DataFailed(appException.message);
+
+      /// dio or timeout and etc error
+      case FetchDataException:
+        return DataFailed(appException.message);
+    }
+  }
+}
