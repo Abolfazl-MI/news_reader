7ec745bf1a2196a401801ba0f4daaa4994fac575
abolfazl <abolfazlmashhadi93@gmail.com>
2023-05-05 21:29:59 +0330

    updated api constants

---
+++ lib/api_constants.dart
@@ -0,0 +1,9 @@
+///for personal usage
+const apiKey = 'bb78d40106a64cd3abea69128a8b3155';
+
+/// api urls
+class ApiUrls {
+  ApiUrls._();
+  static const String baseUrl = 'https://newsapi.org/v2';
+  static const String topHeadLineNews = '/top-headlines';
+}
