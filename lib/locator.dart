651d5fc301e31b7399b1afa1187a911bf8d11446
abolfazl <abolfazlmashhadi93@gmail.com>
2023-05-05 21:29:45 +0330

    updated dio config

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:news_reader/api_constants.dart';
 import 'package:news_reader/common/utils/pref_opreator.dart';
 import 'package:news_reader/features/feature_intro/presentation/bloc/splash_cubit/splash_cubit.dart';
 import 'package:news_reader/features/feature_intro/repositories/spalsh_repository.dart';
@@ -9,7 +10,11 @@ GetIt locator = GetIt.instance;
 
 Future<void> initLocator() async {
   /// networking lib
-  locator.registerSingleton<Dio>(Dio());
+  locator.registerSingleton<Dio>(Dio(BaseOptions(
+    baseUrl: ApiUrls.baseUrl, 
+    contentType: 'application/json', 
+    
+  )));
 
   ///SharedPreferences
   SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
