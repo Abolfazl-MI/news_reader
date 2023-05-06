0a73d6a0207267dcdd9b1f4ffd1410e69d377bbc
abolfazl <abolfazlmashhadi93@gmail.com>
2023-05-06 10:28:51 +0330

    added select topic screen

---
+++ lib/features/feature_select_topic/presentation/screens/select_topic_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SelectTopicScreen extends StatefulWidget {
+  static const String routeName = '/select-topics';
+  const SelectTopicScreen({super.key});
+
+  @override
+  State<SelectTopicScreen> createState() => _SelectTopicScreenState();
+}
+
+class _SelectTopicScreenState extends State<SelectTopicScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
