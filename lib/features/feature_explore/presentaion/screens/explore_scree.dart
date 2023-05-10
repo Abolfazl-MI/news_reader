e0ee5791e60f4d0509f1130ced0195aee55af24a
abolfazl <abolfazlmashhadi93@gmail.com>
2023-05-10 09:57:47 +0330

    feat:add explore screen

---
+++ lib/features/feature_explore/presentaion/screens/explore_scree.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/material.dart';
+
+class ExploreScreen extends StatelessWidget {
+  const ExploreScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'explore Screen'
+        ),
+      ),
+    );
+  }
+}
