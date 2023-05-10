fa614c2b3d015a29d462f9fca48a90e8cb0df7fb
abolfazl <abolfazlmashhadi93@gmail.com>
2023-05-10 09:58:13 +0330

    feat:add book mark screen

---
+++ lib/features/feature_bookmark/presentation/screens/book_mark_screen.dart
@@ -0,0 +1,18 @@
+
+
+import 'package:flutter/material.dart';
+
+class BookMarkScreen extends StatelessWidget {
+  const BookMarkScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'bookMark Screen'
+        ),
+      ),
+    );
+  }
+}
