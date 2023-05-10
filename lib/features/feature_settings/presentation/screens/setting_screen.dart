dd2206b3068012056baac23c5546945fcd847507
abolfazl <abolfazlmashhadi93@gmail.com>
2023-05-10 09:57:27 +0330

    feat: add setting screen

---
+++ lib/features/feature_settings/presentation/screens/setting_screen.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/material.dart';
+
+class SettingScreen extends StatelessWidget {
+  const SettingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Setting Screen'
+        ),
+      ),
+    );
+  }
+}
